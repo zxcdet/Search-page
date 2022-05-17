95e9115767e540bfd379d68a46fa6d12ff62779d
Mykhailo Kisil <jallyface@gmail.com>
2022-05-17 14:28:34 +0300

    make it better

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://api.spaceflightnewsapi.net/v3/articles"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://api.spaceflightnewsapi.net/v3/articles"
+
 };
 
 /*
